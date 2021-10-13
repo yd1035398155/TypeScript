d57aa13413bc1694c519232f44c08d4d9ce18199
Sora <1035398155@qq.com>
2021-10-13 15:55:08 +0800

    u

---
+++ 07.TypeScript类型补充2/01_类型断言as.ts
@@ -0,0 +1,21 @@
+// as 作用 把宽泛的类型转换为具体的类型
+// 1
+// <img id="why" />
+// const el = document.getElementById("why") as HTMLImageElement
+
+// 2
+// class Person {}
+// class Student extends Person {
+//   study() {
+//     console.log("studying")
+//   }
+// }
+// function sayHello(p: Person) {
+//   ;(p as Student).study()
+// }
+// const stu = new Student()
+// sayHello(stu)
+
+// 3 了解 不推荐
+const message = "abc"
+const res: number = message as any as number
